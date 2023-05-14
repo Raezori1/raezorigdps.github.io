1d424b535fbad449ac845a2137448baf193819a4
Raezori1 <133571913+Raezori1@users.noreply.github.com>
2023-05-14 21:18:43 +1000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -108,16 +108,10 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
-                    </p>
-                    <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps. 
                     </p>
                     <p>
                         Have either source audio or clicks/taps in the video. Edited audio only does not count
-                    </p>
-                    <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
                     </p>
                     <p>
                         The recording must also show the player hit the endwall, and display end stats, or the completion will be invalidated.
@@ -126,10 +120,10 @@ export default {
                         Do not use secret routes or bug routes
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        Public LDM's are allowed if one is not present on the actual level
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Once a level falls onto the Legacy List, we will no longer accept records for said level
                     </p>
                 </div>
             </div>
